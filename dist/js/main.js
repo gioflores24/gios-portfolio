df4008d0d333f82f3345e234953f1c0eed2acb64
Giovanni Flores <gdr943@gmail.com>
2019-08-01 17:12:33 -0700

    Menu overlay created

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,7 +2,7 @@
 
 const menuButton = document.querySelector(".menu-button");
 const menu = document.querySelector(".menu");
-const portrait = document.querySelector(".portrait");
+const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
 const navItems = document.querySelectorAll(".nav-item");
 
@@ -11,7 +11,7 @@ menuButton.addEventListener("click", () => {
   if (!showMenu) {
     menuButton.classList.add("close");
     menu.classList.add("show");
-    portrait.classList.add("show");
+    menuNav.classList.add("show");
     menuBranding.classList.add("show");
     navItems.forEach(item => item.classList.add("show"));
     showMenu = true;
@@ -20,7 +20,7 @@ menuButton.addEventListener("click", () => {
 
     menuButton.classList.remove("close");
     menu.classList.remove("show");
-    portrait.classList.remove("show");
+    menuNav.classList.remove("show");
     menuBranding.classList.remove("show");
     navItems.forEach(item => item.classList.remove("show"));
     showMenu = false;
